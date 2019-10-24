e6865bb6d02b61bf10c60db242b94d3a5c974b02
iandrews17 <isaac.andrews22@gmail.com>
2019-10-24 08:36:24 -0400

    Finished the code for Give-it-a-Trie. Found I was trying to be too fancy and decided to start over.

--- Menu.java
+++ Menu.java
@@ -1,67 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+public class Menu extends Trie {
 
-public class Menu extends Trie
-{
-
- public static void main(String[] args){
-  char c = 'a';
-  int numLetters = 0;
-  int count=0;
-  int length;
-  String startingletters;
-
-  Scanner s= new Scanner(System.in);
-
-  System.out.println("Press i insert values");
-  System.out.println("Press q to quit");
-  c=s.next().charAt(0);
-  while(c!='q' || c != 'Q'){
-
-  if(c=='i' || c == 'I'){
-    while(count==0){
-    System.out.println("Number of letters:");
-    String numLettersString=s.next();
-    numLetters = Integer.parseInt(numLettersString);
-    if(!(numLetters<8) && !(numLetters>0)){
-      System.out.println("Must be less than or equal to seven and greater than 0");
-      continue;
-      }
-      count=1;
-      break;
-    }
-    System.out.print("Letters: ");
-    String str=s.next();
-    while (str.length() != numLetters)
-    {
-      System.out.println("Please give the correct amount of letters: ");
-      str = s.next();
-    }
-    char []array= new char[numLetters];
-    for(int n=0; n<numLetters; n++){
-      array[n]=str.charAt(n);
-      array[n] = Character.toUpperCase(array[n]);
-    }
-    System.out.println("Enter the letter(s) you would like the word to start with");
-    startingletters=s.next();
-    startingletters = startingletters.toUpperCase();
-    System.out.println("Enter the length of the word you would want to place on the board");
-    String lengthString=s.next();
-    length = Integer.parseInt(lengthString);
-
-
+  public static void main(String[] args) {
     Trie trie = new Trie();
     trie.createTrie();
-    trie.checkForScores(startingletters, array, length, startingletters.length());
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What letter(s) are in your rack? (separate with spaces)");
+
+    String rack = scan.nextLine();
+    rack.trim();
+    System.out.println("What letter(s) will your word start with? (not separated with spaces)");
+    String start = scan.nextLine();
+    trie.checkForScores(start, rack);
+
+    scan.close();
   }
-
-
-  System.out.println("Press i insert values");
-  System.out.println("Press r to restart");
-  System.out.println("Press q to quit");
-  c = s.next().charAt(0);
-  }
- }
-
 }
--- Trie.java
+++ Trie.java
@@ -2,59 +2,51 @@ import java.lang.*;
 import java.util.*;
 import java.io.*;
 
-class TrieNode
-{
-  public TrieNode [] children;
+class TrieNode {
+  public TrieNode[] children;
   public char letter;
   public boolean isWord;
 
-  public TrieNode(char letter)
-  {
+  public TrieNode(char letter) {
     this.letter = letter;
     children = new TrieNode[26];
+    this.isWord = false;
   }
 }
 
-public class Trie
-{
+public class Trie {
   private TrieNode root;
 
-  public Trie()
-  {
+  public Trie() {
     root = new TrieNode(' ');
   }
 
-  public void createTrie()
-  {
-    try
-    {
+  public void createTrie() {
+    try {
       Scanner scan = new Scanner(new File("Scrabble.txt"));
-      while(scan.hasNext())
-      {
+      while (scan.hasNext()) {
         String word = scan.next();
         this.insert(word);
       }
+      scan.close();
     }
 
-    catch(Exception exc)
-    {
-      System.out.println("No idea");
+    catch (Exception exc) {
+      System.out.println("The file could not be found or could not be opened");
     }
   }
 
-  // Inserts a word into the Trie. If the path does not currently exist we create the
+  // Inserts a word into the Trie. If the path does not currently exist we create
+  // the
   // path (new nodes).
-  public void insert(String word)
-  {
+  public void insert(String word) {
     TrieNode current = root;
     int length = word.length();
     char letter;
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
       letter = word.charAt(i);
       int index = (int) letter - 'A';
-      if (current.children[index] == null)
-      {
+      if (current.children[index] == null) {
         current.children[index] = new TrieNode(letter);
       }
 
@@ -64,110 +56,85 @@ public class Trie
     current.isWord = true;
   }
 
-  public void printTrie()
-  {
+  public void printTrie() {
     printTrie(root, "");
   }
 
-  private void printTrie(TrieNode node, String word)
-  {
-    if (node == null)
-    {
+  private void printTrie(TrieNode node, String word) {
+    if (node == null) {
       return;
     }
 
     word += node.letter;
-    if (node.isWord)
-    {
+    if (node.isWord) {
       System.out.println(word);
     }
 
-    for (int i = 0; i < 26; i++)
-    {
-      if (node.children[i] != null)
-      {
+    for (int i = 0; i < 26; i++) {
+      if (node.children[i] != null) {
         printTrie(node.children[i], word);
       }
     }
   }
 
-  public void checkForScores(String word,char [] deck, int maxLength, int currentLength)
-  {
-    String bestWord = "";
-    boolean [] used = new boolean [deck.length];
-    StringBuilder builder = new StringBuilder(word);
-    TrieNode startingNode = checkforStartingLetters(builder, maxLength, currentLength, root, bestWord);
-    builder.deleteCharAt(word.length() - 1);
-    bestWord = checkForScores(builder, deck, used, maxLength, currentLength, startingNode, bestWord);
-    System.out.println("Your best word is " + bestWord + " for " + addLettersTogether(bestWord) + " points");
+  public void checkForScores(String start, String rack) {
+    String buildWord = "";
+    String bestWord = start;
+    StringBuilder builder = new StringBuilder(rack);
+    TrieNode starting = startsWith(start);
+    if (starting == null) {
+      System.out.println("No words that begin with " + start + " were found");
+    } else {
+      bestWord = checkForScores(builder, buildWord, bestWord, starting);
+      System.out.println("Your best word is " + bestWord + " for " + addLettersTogether(bestWord) + " points");
+    }
   }
 
-  private TrieNode checkforStartingLetters(StringBuilder word, int maxLength, int currentLength, TrieNode root, String bestWord)
-  {
+  private TrieNode startsWith(String start) {
     TrieNode current = root;
-    for (int i = 0; i < word.length(); i++)
-    {
-      char letter;
-      letter = word.charAt(i);
-      int index = (int) letter - 'A';
-      if (current.children[index] == null)
-      {
-        return current;
+    int length = start.length();
+    for (int i = 0; i < length; i++) {
+      if (current.children[start.charAt(i) - 'A'] != null) {
+        current = current.children[start.charAt(i) - 'A'];
+      } else {
+        return null;
       }
-
-      current = current.children[index];
     }
 
     return current;
   }
 
-  private String checkForScores(StringBuilder word, char [] deck, boolean [] used, int maxLength, int currentLength, TrieNode node, String bestWord)
-  {
-
-
-    if (currentLength > maxLength)
-    {
-      return bestWord;
+  private String checkForScores(StringBuilder rack, String buildWord, String bestWord,
+      TrieNode currentNode) {
+    if (currentNode == null) {
+      return "";
     }
-    if (currentLength == maxLength && node.isWord && word.length() == maxLength - 1)
-    {
-      word.append(node.letter);
-
-      String newWord = word.toString();
-      if (addLettersTogether(newWord) > addLettersTogether(bestWord))
-      {
-        bestWord = newWord;
-      }
-      System.out.println("New best word is " + bestWord);
-      return bestWord;
+    if (currentNode.letter != ' ') {
+      buildWord += currentNode.letter;
     }
 
-    for (int i = 0; i < 26; i++)
-    {
-      if (node.children[i] != null)
-      {
-        for (int j = 0; j < deck.length; j++)
-        {
-          if (deck[j] == (char) i + 'A' && (used[j] == false));
-          {
+    if (currentNode.isWord) {
 
-            used[j] = true;
-            for (int k = 0; k < deck.length; k++)
-            {
-              if (deck[k] == node.letter)
-              {
-                word.append(node.letter);
-                System.out.println(word);
-              }
-            }
-            bestWord = checkForScores(word, deck, used, maxLength, currentLength + 1, node.children[i], bestWord);
-            if (node != root)
-            {
-              word.deleteCharAt(word.length() - 1);
-            }
+      String tempWord = buildWord;
+      // System.out.println();
+      // System.out.println("Best word is: " + bestWord);
+      // System.out.println("Temp word is: " + tempWord);
+      // System.out.println(currentNode.letter);
+      // System.out.println(rack);
+      bestWord = (addLettersTogether(tempWord) > addLettersTogether(bestWord) ? tempWord : bestWord);
 
-            used[j] = false;
-          }
+    }
+    if (rack.length() == 0) {
+      return bestWord;
+    }
+    for (int i = 0; i < currentNode.children.length; i++) {
+      if (currentNode.children[i] != null) {
+        int index = rack.toString().indexOf((char) ('A' + i));
+        if (index > -1) {
+          String tempWord = checkForScores(rack.deleteCharAt(index), buildWord, bestWord,
+              currentNode.children[i]);
+          bestWord = (addLettersTogether(tempWord) > addLettersTogether(bestWord) ? tempWord : bestWord);
+          rack.append((char) ('A' + i));
         }
       }
     }
@@ -175,14 +142,24 @@ public class Trie
     return bestWord;
   }
 
-  public int addLettersTogether(String word)
-  {
-    int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+  public int addLettersTogether(StringBuilder word) {
+    int[] points = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
     int length = word.length();
     int total = 0;
 
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
+      total += points[word.charAt(i) - 'A'];
+    }
+
+    return total;
+  }
+
+  public int addLettersTogether(String word) {
+    int[] points = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+    int length = word.length();
+    int total = 0;
+
+    for (int i = 0; i < length; i++) {
       total += points[word.charAt(i) - 'A'];
     }
 
